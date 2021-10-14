21bfca5d0fc348ef7ae5988141fe6f91df5425ed
Vladyslav Prosianyk <vladyslav.prosianyk@gmail.com>
2021-10-14 18:40:34 +0300

    Initial Commit

---
+++ Vladyslav Prosianyk BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vladyslav Prosianyk BMI
+//
+//  Created by Vladyslav Prosianyk on 14.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
